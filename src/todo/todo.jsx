3e3d923d5b02d01e16259e1dbe3a958fd00e41fd
Jonathan C. Calixto <jonathan@rarolabs.com.br>
2018-10-27 14:57:00 -0300

    Consultation and Exclusion of TODOs

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -12,8 +12,19 @@ export default class Todo extends Component {
         super(props);
         this.state = {description: '', list: [] };
 
-        this.handleAdd = this.handleAdd.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+
+        this.refresh();
+    }
+    refresh() {
+        axios.get(`${URL}?sort=-createdAt`)
+            .then(response =>  this.setState({
+                ...this.state,
+                description: '',
+                list: response.data
+            }));
     }
     handleChange (event) {
         this.setState({
@@ -24,7 +35,11 @@ export default class Todo extends Component {
     handleAdd () {
         const description = this.state.description;
         axios.post(URL, { description })
-             .then(resp => console.log('Funcionou!'));
+             .then(response => this.refresh());
+    };
+    handleRemove (todo) {
+        axios.delete(`${URL}/${todo._id}`)
+             .then(response => this.refresh());
     };
     render () {
         return (
@@ -34,7 +49,9 @@ export default class Todo extends Component {
                           handleChange={this.handleChange}
                           handleAdd={this.handleAdd}
                 />
-                <TodoList />
+                <TodoList list={this.state.list}
+                          handleRemove={this.handleRemove}
+                />
             </div>
         )
     };
